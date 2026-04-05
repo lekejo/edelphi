bc836ec5c9e8cbc781c3c5367fb59027ea28776b
lekejo <121046982+lekejo@users.noreply.github.com>
2026-04-05 10:47:06 +0400

    Fix WildFly interface binding at runtime - pass -b 0.0.0.0 and -bmanagement 0.0.0.0 to force bind to any interface instead of eth0

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,10 @@
 echo "Starting server..." &&
 rm -fR /opt/jboss/wildfly/standalone/configuration/standalone_xml_history &&
-exec /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 --server-config=standalone-full-ha.xml -Djboss.node.name=$(hostname) ${WILDFLY_EXTRA_OPTS}
+exec /opt/jboss/wildfly/bin/standalone.sh \
+  -b 0.0.0.0 \
+  -bmanagement 0.0.0.0 \
+  -bprivate 0.0.0.0 \
+  -bunsecure 0.0.0.0 \
+  --server-config=standalone-full-ha.xml \
+  -Djboss.node.name=$(hostname) \
+  ${WILDFLY_EXTRA_OPTS}
